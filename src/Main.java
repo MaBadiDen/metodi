0c10317318a8f255ce52bce7fa7c8a0f1b7a9b02
MaBadiDen <kaba4ok.den@gmail.com>
2022-08-06 22:41:52 +0300

    Готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.time.LocalDate;
+
 public class Main {
+    public static boolean isVisoc(int year){
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
+
+    public static void isOlder(int currentYear, int year, int os) {
+        if (os == 0 && year == currentYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (os == 0 && year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (os == 1 && year == currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (os == 1 && year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static int getDays(int distance) {
+        if(distance <= 20) {
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year = 400;
+        int os = 0;
+        int currentYear = LocalDate.now().getYear();
+        int distance = 60;
+        if(isVisoc(year)) {
+            System.out.println(year + " - високосный год");
+        } else {
+            System.out.println(year + " - не високосный год");
+        }
+        isOlder(currentYear, year, os);
+        System.out.println("Потребуется дней - " + getDays(distance));
     }
 }
